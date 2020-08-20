897cc66a263597d3719f26c2753c42886e66a65e
Gabriela Cabrera <gaby.cabrera9@gmail.com>
2020-08-20 10:56:55 -0400

    bug fix

--- controllers/flights.js
+++ controllers/flights.js
@@ -14,6 +14,7 @@ function newFlight(req, res, next) {
   }
 
   function create(req, res) {
+    req.body.departs = req.body.flightDate;
     for (let key in req.body) {
       if (req.body[key] === '') delete req.body[key];}
     Flight.create(req.body)
